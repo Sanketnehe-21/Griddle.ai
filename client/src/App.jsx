71780511c10beed3cdb12ad1e82d0c349948e092
Sanketnehe-21 <sanketnehe21@gmail.com>
2025-07-17 16:37:25 +0530

    navbar component added to Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,11 @@ import Layout from './pages/Layout.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import WriteArticle from './pages/WriteArticle.jsx'
 import BlogTitles from './pages/BlogTitles.jsx'
+import GenerateImages from './pages/GenerateImages.jsx'
+import RemoveObject from './pages/RemoveObject.jsx'
+import RemoveBackground from './pages/RemoveBackground.jsx'
+import ReviewResume from './pages/ReviewResume.jsx'
+import Community from './pages/Community.jsx'
 const App = () => {
   return (
     <div>
@@ -14,6 +19,11 @@ const App = () => {
         <Route index element={<Dashboard/>}/>
         <Route path="write-article" element={<WriteArticle/>}/>
         <Route path="blog-article" element={<BlogTitles/>}/>
+        <Route path="generate-image" element={<GenerateImages/>}/>
+        <Route path="remove-object" element={<RemoveObject/>}/>
+        <Route path="remove-background" element={<RemoveBackground/>}/>
+        <Route path="review-resume" element={<ReviewResume/>}/>
+        <Route path="community" element={<Community/>}/>
         
         </Route>
       </Routes>
